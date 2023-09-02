6291af854aab773c315ffc821115f39602f98937
krokerdile <crazykroker@gmail.com>
2023-09-02 22:28:00 +0900

    prompt main에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import "./App.css";
 import styled from "styled-components";
 import { useRef, useState } from "react";
 import "./fonts/Font.css";
-import Slider from "react-slick";
+import { Configuration, OpenAIApi } from "openai";
+
+// import Slider from "react-slick";
+
+const KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
 function App() {
   const input = useRef(null);
@@ -14,83 +18,79 @@ function App() {
       setValue("");
     }
   };
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+
+  const configuration = new Configuration({
+    apiKey: KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const result = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: value,
+        temperature: 0.5,
+        max_tokens: 100,
+      });
+
+      setResponse(result.data.choices[0].text);
+      setValue(""); // 입력 필드를 지웁니다.
+    } catch (e) {
+      setResponse("Something went wrong. Please try again.");
+    }
+    setLoading(false);
   };
+
+  const handleReset = () => {
+    setResponse("");
+    setValue("");
+  };
+
   return (
     <>
       <MainBox>
         <Title>Contract Assistant</Title>
-        <p>현재 검색어는 {value}입니다</p>
-        <InputBox
-          type="text"
-          ref={input}
-          onChange={() => {
-            setValue(input.current.value);
-          }}
-        />
-        <button type="button" onClick={handleClick}>
-          Click to Reset
+        {/* <p>
+          현재 검색어는 <strong>{value}</strong>입니다
+        </p> */}
+        <div className="response">
+          <strong>Response:</strong>
+          <p>{response}</p>
+        </div>
+        <InputWrapper>
+          <InputBox
+            type="text"
+            ref={input}
+            onChange={() => {
+              setValue(input.current.value);
+            }}
+          />
+
+          <SearchButton type="button" onClick={handleClick}>
+            Reset
+          </SearchButton>
+          <SearchButton type="button" onClick={handleSubmit}>
+            Search
+          </SearchButton>
+        </InputWrapper>
+
+        <button disabled={loading} type="submit">
+          {loading ? "Generating..." : "Generate"}
         </button>
-        <SliderBox>
-          <Slider {...settings}>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
-            </div>
-            <div>
-              <h3>7</h3>
-            </div>
-            <div>
-              <h3>8</h3>
-            </div>
-          </Slider>
-        </SliderBox>
+        <button type="button" onClick={handleReset}>
+          Reset
+        </button>
+        <ButtonList>
+          <TopicButton>근로계약서</TopicButton>
+          <TopicButton>전세계약서</TopicButton>
+          <TopicButton>프리랜서계약서</TopicButton>
+        </ButtonList>
       </MainBox>
     </>
   );
@@ -110,10 +110,10 @@ const MainBox = styled.div`
   flex-direction: column;
 `;
 
-const SliderBox = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+// const SliderBox = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `;
 
 const InputBox = styled.input`
   height: 2rem;
@@ -123,4 +123,30 @@ const InputBox = styled.input`
   border-radius: 0.5rem;
   margin-bottom: 1rem;
   border: 0.1rem black solid;
+`;
+
+const SearchButton = styled.button`
+  height: 2rem;
+  padding-bottom: 1.6rem;
+`;
+
+const TopicButton = styled.button`
+  width: 30%;
+`;
+
+const ButtonList = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin-top: 1rem;
+`;
+
+const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 0.5rem;
+  flex-direction: row;
+  margin-top: 1rem;
 `;
